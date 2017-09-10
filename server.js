dbfa3f2c740d2ccf59abc5e74772e0528b8f4b60
arshavskiy <pasha.websbee@gmail.com>
2017-09-10 08:08:29 +0300

    route code simplified

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.static('views'));
+app
+    .use('/public', express.static(process.cwd() + '/public'))
+    .use(express.static('views'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', routes.index);
-app.get('/Home', routes.index);
-app.get('/About', routes.about);
+app
+    .get('/', routes.index)
+    .get('/Home', routes.index)
+    .get('/About', routes.about);
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port + '...');
